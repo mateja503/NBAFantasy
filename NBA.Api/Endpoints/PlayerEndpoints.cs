e0e4fc6402b612838ef9beb999a1e3c516f4e189
mateja503 <matejanikolikj503@gmail.com>
2026-02-06 00:20:01 +0100

    return only one player endpoint created

--- NBA.Api/Endpoints/PlayerEndpoints.cs
+++ NBA.Api/Endpoints/PlayerEndpoints.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NBA.Data.Context;
 using NBA.Data.Entities;
+using System;
 
 namespace NBA.Api.Endpoints
 {
@@ -21,16 +22,16 @@ namespace NBA.Api.Endpoints
             .WithDescription("This endpoint returns a comprehensive list of all NBA players in the database.")
             .Produces<List<Player>>(StatusCodes.Status200OK);
 
-            //group.MapGet("/{id:int}", async (int id, IPlayerService playerService) =>
-            //{
-            //    var player = await playerService.GetPlayerByIdAsync(id);
-            //    return player is not null ? Results.Ok(player) : Results.NotFound();
-            //})
-            //.WithName("GetPlayerById")
-            //.WithSummary("Retrieves a specific NBA player by their ID.")
-            //.WithDescription("This endpoint returns the details of an NBA player based on the provided player ID.")
-            //.Produces<Player>(StatusCodes.Status200OK)
-            //.Produces(StatusCodes.Status404NotFound);
+            player.MapGet("/{id:int}", async (int id, NbaFantasyContext context) =>
+            {
+                var player = await context.GetAllPlayers().AsNoTracking().Where(u=>u.Playerid == id).ToListAsync();
+                return player is not null ? Results.Ok(player) : Results.NotFound();
+            })
+            .WithName("GetPlayerById")
+            .WithSummary("Retrieves a specific NBA player by their ID.")
+            .WithDescription("This endpoint returns the details of an NBA player based on the provided player ID.")
+            .Produces<Player>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status404NotFound);
             return player;
         }
     }
