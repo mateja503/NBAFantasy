c4704bbe02dee6fa68d8faba66e2c74974026ecf
mateja503 <matejanikolikj503@gmail.com>
2026-02-10 12:47:21 +0100

    added ranglist endpoints

---
+++ NBA.Api/Endpoints/RangListEndpoints.cs
@@ -0,0 +1,15 @@
+﻿namespace NBA.Api.Endpoints
+{
+    public static class RangListEndpoints
+    {
+        public static IEndpointRouteBuilder MapRangListEndpoints(this IEndpointRouteBuilder builder)
+        {
+            var ranglist = builder.MapGroup("/ranglist");
+            ranglist.MapGet("/", () =>
+            {
+                return Results.Ok("This is the ranglist endpoint.");
+            }).WithTags("ranglist");
+            return ranglist;
+        }
+    }
+}
