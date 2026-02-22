732801e5fcfb52080ba1434799f9e2e7e1a67575
mateja503 <matejanikolikj503@gmail.com>
2026-02-22 22:59:39 +0100

    gegt active players after 2003

--- NBA.Service/PlayerFilter.cs
+++ NBA.Service/PlayerFilter.cs
@@ -12,7 +12,7 @@ namespace NBA.Service
         {
             return playersInfo.Where(player => 
             {
-                if (player.draft_year <= 2003 || string.IsNullOrEmpty(player.position) || string.IsNullOrEmpty(player.jersey_number))
+                if (player.draft_year < 2003 || string.IsNullOrEmpty(player.position) || string.IsNullOrEmpty(player.jersey_number))
                     return false;
 
                 return true;
