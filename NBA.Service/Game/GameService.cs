dc80fda496d56f20d2b9cec6c641925ddd9b315a
mateja503 <matejanikolikj503@gmail.com>
2026-04-26 00:21:09 +0200

    remove the usage of the statice BackGroupJob from hangfilre

--- NBA.Service/Game/GameService.cs
+++ NBA.Service/Game/GameService.cs
@@ -5,9 +5,12 @@ using NBA.Service.Player;
 
 namespace NBA.Service.Game
 {
-    public class GameService(BallDontLieClient ballDontLieClient)
+    public class GameService(BallDontLieClient ballDontLieClient, IBackgroundJobClient jobClient)
     {
         private readonly BallDontLieClient _ballDontLieClient = ballDontLieClient;
+
+        public IBackgroundJobClient _jobClient = jobClient;
+
         public async Task<List<GameInfoResponse>> TodaysGames(CancellationToken cancellationToken)
         {
             var games = await _ballDontLieClient.GetTodaysGames(cancellationToken);
@@ -16,7 +19,7 @@ namespace NBA.Service.Game
             {
                 DateTimeOffset gameFinishes = new DateTimeOffset(game.datetime).AddHours(4);
 
-                BackgroundJob.Schedule<PlayerService>(
+                _jobClient.Schedule<PlayerService>(
                     playerService => playerService.GetPlayersGameStats(game.id,game.home_team.id,
                     game.visitor_team.id, game.home_team.full_name,game.visitor_team.full_name, CancellationToken.None),
                     gameFinishes);
