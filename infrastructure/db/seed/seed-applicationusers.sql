ce1cf8c1b6ca9ccbd7bc6a4514e79535fa3edbad
mateja503 <matejanikolikj503@gmail.com>
2026-05-27 19:18:43 +0200

    seed more application users

--- infrastructure/db/seed/seed-applicationusers.sql
+++ infrastructure/db/seed/seed-applicationusers.sql
@@ -13,7 +13,9 @@ BEGIN
         ('dunk_master', 'Test.123', 'dunkmaster@example.com', 3200, 10),
         ('court_vision', 'Test.123', 'pointgod@example.com', 0, 1),
         ('celtics_pride', 'Test.123', 'greenrun@example.com', 850, 3),
-        ('buzzer_beater', 'Test.123', 'clutch@example.com', 2100, 7);
+        ('buzzer_beater', 'Test.123', 'clutch@example.com', 2100, 7),
+        ('sk_mzt', 'Test.123', 'mzt@example.com', 443, 4),
+        ('czv_48', 'Test.123', 'czv@example.com', 117, 8);
         
         RAISE NOTICE 'Table nba.applicationuser exists. Seed data inserted successfully.';
     ELSE
