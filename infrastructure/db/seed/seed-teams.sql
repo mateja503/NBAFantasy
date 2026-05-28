05805a47f975b6bfe3784fe51d856a759ce459ec
mateja503 <matejanikolikj503@gmail.com>
2026-05-28 12:10:39 +0200

    seed team

--- infrastructure/db/seed/seed-teams.sql
+++ infrastructure/db/seed/seed-teams.sql
@@ -16,14 +16,14 @@ BEGIN
         ) 
         VALUES 
             ('Bulls', 1, 1,0,0,false,1),
-            ('Cleveland', 1, 1,0,0,false,1),
             ('Warriors', 2, 1,0,0,false,2),
             ('Boston', 3, 1,0,0,false,3),
             ('Knicks', 4, 1,0,0,false,4),
             ('Atlanta', 5, 1,0,0,false,5),
             ('Kumanovo', 6, 1,0,0,false,6),
             ('Skopje', 1, 1,0,0,false,7),
-            ('SRB', 1, 1,0,0,false,8);
+            ('SRB', 1, 1,0,0,false,8),
+            ('Cleveland', 1, 1,0,0,false,1);
             
         RAISE NOTICE '8 rows successfully inserted into nba.team.';
     ELSE
